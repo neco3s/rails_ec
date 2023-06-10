9e41c4e4cc1279a4fa0bef8aadf2d936053da146
neco3s <neco3coffee@gmail.com>
2023-06-10 09:23:13 +0900

    imageの追加 productのseedデータの作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -6,3 +7,12 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+5.times do |i|
+  product = Product.create(name: "Shop item template-#{i}", code: "SKU: BST-#{i}",
+                           description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium at dolorem quidem modi. Nam sequi consequatur obcaecati excepturi alias magni, accusamus eius blanditiis delectus ipsam minima ea iste laborum vero?', price: 4000)
+  product.image.attach(io: File.open("./app/assets/images/image_#{i}.jpg"),
+                       filename: "image_#{i}.jpg",
+                       content_type: 'image/png')
+  product.save
+end
