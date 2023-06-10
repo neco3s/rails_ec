a1be25abb520d8cc08d282324360fd2d5c3c637f
neco3s <neco3coffee@gmail.com>
2023-06-10 15:47:05 +0900

    seed修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,10 +10,10 @@
 
 # rubocop:disable Layout/LineLength
 5.times do |i|
-  product = Product.create(name: "Shop item template-#{i - 1}", code: "SKU: BST-#{i - 1}",
-                           description: "#{i - 1}Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium at dolorem quidem modi. Nam sequi consequatur obcaecati excepturi alias magni, accusamus eius blanditiis delectus ipsam minima ea iste laborum vero?", price: 4000)
-  product.image.attach(io: File.open("./app/assets/images/image_#{i - 1}.jpg"),
-                       filename: "image_#{i - 1}.jpg",
+  product = Product.create(name: "Shop item template-#{i}", code: "SKU: BST-#{i}",
+                           description: "#{i}Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium at dolorem quidem modi. Nam sequi consequatur obcaecati excepturi alias magni, accusamus eius blanditiis delectus ipsam minima ea iste laborum vero?", price: 4000)
+  product.image.attach(io: File.open("./app/assets/images/image_#{i}.jpg"),
+                       filename: "image_#{i}.jpg",
                        content_type: 'image/png')
   product.save
 end
