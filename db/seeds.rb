2e9a0e031daaabffd93633cfd735a3508a1c5ea9
neco3s <neco3coffee@gmail.com>
2023-06-10 11:16:34 +0900

    seedデータの修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,10 +9,10 @@
 #   Character.create(name: "Luke", movie: movies.first)
 
 5.times do |i|
-  product = Product.create(name: "Shop item template-#{i}", code: "SKU: BST-#{i}",
+  product = Product.create(name: "Shop item template-#{i - 1}", code: "SKU: BST-#{i - 1}",
                            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium at dolorem quidem modi. Nam sequi consequatur obcaecati excepturi alias magni, accusamus eius blanditiis delectus ipsam minima ea iste laborum vero?', price: 4000)
-  product.image.attach(io: File.open("./app/assets/images/image_#{i}.jpg"),
-                       filename: "image_#{i}.jpg",
+  product.image.attach(io: File.open("./app/assets/images/image_#{i - 1}.jpg"),
+                       filename: "image_#{i - 1}.jpg",
                        content_type: 'image/png')
   product.save
 end
