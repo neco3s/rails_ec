dc681b85de3bdf753d66d4635abaa519ac8ce214
neco3s <neco3coffee@gmail.com>
2023-06-11 14:55:30 +0900

    不要なmigrationファイルの削除

--- db/migrate/20230528051040_init_schema.rb
+++ db/migrate/20230528051040_init_schema.rb
@@ -8,23 +8,11 @@ class InitSchema < ActiveRecord::Migration[7.0]
       t.string :code, null: false, comment: '商品コード'
       t.text :description, null: false, comment: '商品説明'
       t.integer :price, null: false, comment: '価格'
-      t.string :image_url, null: false, comment: '商品画像URL'
 
       t.timestamps
 
       # インデックス作成(default)と一意性制約=>シーケンシャルスキャンを防ぐ
       t.index(:id, unique: true)
-    end
-
-    create_table :promo_codes do |t| # プロモコードテーブル
-      t.string :code, null: false, comment: 'プロモーションコード'
-      t.float :discount, null: false, comment: 'ディスカウント'
-      t.boolean :used, default: false, null: false, comment: '使用済み'
-
-      t.timestamps
-
-      # インデックス作成(default)と一意性制約=>シーケンシャルスキャンを防ぐ
-      t.index(:code, unique: true)
     end
   end
 end
--- db/migrate/20230528120023_remove_image_url_from_products.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class RemoveImageUrlFromProducts < ActiveRecord::Migration[7.0]
-  def change
-    remove_column :products, :image_url, :string
-  end
-end
