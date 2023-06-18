8fd504865e55d930320b8f1ae7613740ba39d07d
neco3s <neco3coffee@gmail.com>
2023-06-18 22:28:02 +0900

    admin/products resourcesの作成routes.rbの修正

--- config/routes.rb
+++ config/routes.rb
@@ -15,4 +15,8 @@
 Rails.application.routes.draw do
   root to: 'products#index'
   resources :products, only: %i[index show]
+
+  namespace :admin do
+    resources :products, except: :show
+  end
 end
