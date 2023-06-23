404a4fbd2b612bba22bd81c3c632be4e07aa73df
neco3s <neco3coffee@gmail.com>
2023-06-23 23:35:38 +0900

    新規作成ページにエラーメッセージを表示、product modelにvalidation追加

--- app/models/product.rb
+++ app/models/product.rb
@@ -14,5 +14,11 @@
 #  quantity    :integer          not null
 #
 class Product < ApplicationRecord
+  validates :name, :code, :description, :price, :quantity, presence: true # 空でないことを想定
+  # validates :code, uniqueness: true # 重複しないJANコードを想定
+  # validates :name, length: { maximum: 25 } # 25文字以内の商品名を想定
+  # validates :code, length: { maximum: 13 } # 13桁のJANコードを想定
+  # validates :description, length: { maximum: 300 } # 300文字以内の商品説明を想定
+
   has_one_attached :image
 end
