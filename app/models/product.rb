a4d858a055bb1dc35fb6bc7bef328726156e02ac
neco3s <neco3coffee@gmail.com>
2023-06-25 19:26:23 +0900

    Productモデルにvalidatesを追加

--- app/models/product.rb
+++ app/models/product.rb
@@ -15,12 +15,10 @@
 #
 class Product < ApplicationRecord
   validates :name, :code, :description, :price, :quantity, presence: true # 空でないことを想定
-  # TODO: ja.ymlが正常に動作したら以下の記述をコメントアウトを解除する
-
-  # validates :code, uniqueness: true # 重複しないJANコードを想定
-  # validates :name, length: { maximum: 25 } # 25文字以内の商品名を想定
-  # validates :code, length: { maximum: 13 } # 13桁のJANコードを想定
-  # validates :description, length: { maximum: 300 } # 300文字以内の商品説明を想定
+  validates :code, uniqueness: true # 重複しないJANコードを想定
+  validates :name, length: { maximum: 25 } # 25文字以内の商品名を想定
+  validates :code, length: { maximum: 13 } # 13桁のJANコードを想定
+  validates :description, length: { maximum: 300 } # 300文字以内の商品説明を想定
 
   has_one_attached :image
 end
