7bbc1d704114bb76bbef68d976bffdc9327d921b
neco3s <neco3coffee@gmail.com>
2023-06-25 22:31:20 +0900

    validation修正

--- app/models/product.rb
+++ app/models/product.rb
@@ -17,7 +17,7 @@ class Product < ApplicationRecord
   validates :name, :code, :description, :price, :quantity, :discount, presence: true # 空でないことを想定
   validates :code, uniqueness: true # 重複しないJANコードを想定
   validates :name, length: { maximum: 25 } # 25文字以内の商品名を想定
-  validates :code, length: { maximum: 13 } # 13桁のJANコードを想定
+  validates :code, length: { maximum: 14 } # 13桁のJANコードを想定
   validates :description, length: { maximum: 300 } # 300文字以内の商品説明を想定
   validates :price, numericality: { greater_than_or_equal_to: 0 } # 0以上の価格を想定
   validates :quantity, numericality: { greater_than_or_equal_to: 0 } # 0以上の在庫数を想定
