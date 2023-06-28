2cef59031f33642b118b8ba45f756d592418e655
neco3s <neco3coffee@gmail.com>
2023-06-28 09:16:11 +0900

    productモデルのテスト文を作成

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -14,8 +14,67 @@
 #  updated_at  :datetime         not null
 #  quantity    :integer          default(0), not null
 #
-# require 'rails_helper'
+require 'rails_helper'
 
-# RSpec.describe Product, type: :model do
-#   pending "add some examples to (or delete) #{__FILE__}"
-# end
+RSpec.describe Product, type: :model do
+  it 'is valid with name, code, description, price, quantity, and discount' do
+
+  end
+
+  it 'is invalid without a name' do
+
+  end
+
+  it 'is invalid without a code' do
+
+  end
+
+  it 'is invalid without a description' do
+
+  end
+
+  it 'is invalid without a price' do
+
+  end
+
+  it 'is invalid without a quantity' do
+
+  end
+
+  it 'is invalid without a discount' do
+
+  end
+
+  it 'is invalid with a duplicate code' do
+
+  end
+
+  it 'is invalid with a name that has more than 25 characters' do
+
+  end
+
+  it 'is invalid with a code that has more than 14 characters' do
+
+  end
+
+  it 'is invalid with a description that has more than 300 characters' do
+
+  end
+
+  it 'is invalid with a price that is less than 0' do
+
+  end
+
+  it 'is invalid with a quantity that is less than 0' do
+
+  end
+
+  it 'is invalid with a discount that is less than 0' do
+
+  end
+
+  it 'is invalid with a discount that is more than 100' do
+    
+  end
+
+end
