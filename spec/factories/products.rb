913b34f5b58b6aba9f81db8aafdf2d453709ea70
neco3s <neco3coffee@gmail.com>
2023-06-28 11:25:33 +0900

    FactoryBotで生成されるproductのcodeはユニークなのでsequenceを用いて複数のproductを生成した際にcodeが重複しないようにする

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -3,7 +3,7 @@
 FactoryBot.define do
   factory :product do
     name { '商品名' }
-    code { '1234567890123' }
+    sequence(:code) { |n| "1234567890-#{n}" }
     description { '商品説明' }
     price { 100 }
     quantity { 10 }
