ebbae68f3b6d375ba275603484dd0969e2196cc2
neco3s <neco3coffee@gmail.com>
2023-07-23 09:44:46 +0900

    DBを読み書きする際に、DBに記録されている時間をどのタイムゾーンで読み込むかの設定

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,7 @@ module Myapp
     # in config/environments, which are processed later.
     #
     config.time_zone = 'Tokyo'
-    # config.active_record.default_timezone = :local
+    config.active_record.default_timezone = :local
     # config.eager_load_paths << Rails.root.join("extras")
     config.generators do |g|
       g.test_framework :rspec,
